c92efd4cf98aab905c85ca8b8569945154a2a273
brock810 <130688727+brock810@users.noreply.github.com>
2023-05-07 16:03:33 -0500

    another file

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FactoryPattern
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Please type the number of tires on this vehicle");
+
+            int numTires = int.Parse(Console.ReadLine());
+            IVehicle vehicle = VehicleFactory.GetVehicle(numTires);
+            vehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class VehicleFactory
+    {
+       
+        public static IVehicle GetVehicle (int numTires)
+        {
+            switch (numTires)
+            {
+                case 2:
+                    return new MotorCycle();
+                
+                case 3:
+                    return new Car();
+               
+                case 4: return new SUV();
+                
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+    }
+}
